52c5309dbe27f26abd74bb174bb5bc41caba6fc4
fpalmisa <fabio.palmisano@epfl.ch>
2024-04-24 21:06:49 +0200

    wheel chart, to fix the switch button,

--- src/website/js/statistics.js
+++ src/website/js/statistics.js
@@ -1,9 +1,13 @@
+var season_actual = 'Winter';
+
 (async () => {
     try {
       // Fetch CSV data
       const response = await fetch('../../data/olympic_results_join.csv');
       const csvData = await response.text();
-  
+      // To link with the switch of current season
+
+      console.log(season_actual);
       // Parse CSV data
       const parsedData = d3.csvParse(csvData);
   
@@ -13,11 +17,14 @@
   
       // Iterate over the parsed data to create the labels and parents arrays
       parsedData.forEach(row => {
-        const season = row.season;
         const gameName = row.game_name;
         const disciplineTitle = row.discipline_title;
-        const eventTitle = row.event_title;
+        //const eventTitle = row.event_title;
+        const season = row.game_season;
 
+        if (season !== season_actual) {
+          return;
+        }
 
         // Add game name to labels if not already present
         if (!labels.includes(gameName)) {
@@ -26,14 +33,13 @@
         }
   
         // Add discipline title to labels if not already present
-        if (!labels.includes(disciplineTitle)) {
+
           labels.push(disciplineTitle);
           parents.push(gameName);
-        }
   
         // Add event title to labels
-        labels.push(eventTitle);
-        parents.push(disciplineTitle);
+        //labels.push(eventTitle);
+        //parents.push(disciplineTitle);
       });
   
       // Create sunburst data object
@@ -48,10 +54,10 @@
   
       // Define layout
       const layout = {
-        margin: { l: 0, r: 0, b: 0, t: 0 },
-        width: 800,
-        height: 800
-      };
+        margin: { l: 100, r: 100, b: 200, t: 50 },
+        width: 800, // Adjust this percentage as needed
+        height: 800 // Adjust this percentage as needed
+    };
   
       // Plot sunburst chart
       Plotly.newPlot('myDiv', data, layout);
@@ -60,4 +66,14 @@
     }
   })();
   
-  
+
+  var switchElement = document.getElementById('season-switch');
+
+  switchElement.addEventListener('change', function() {
+    var newSeason = switchElement.checked ? 'Winter' : 'Summer';
+    console.log(newSeason);
+    if (newSeason !== season_actual) {
+      season_actual = newSeason;
+      console.log("Season changed to", season_actual);
+    }
+  });
